7efb34a19ddcf5d9e8f6bd7ffd53cde6dbf10a7a
rg698dgs <darthrevepsg@hotmail.com>
2024-05-12 09:39:22 +0700

    First commit

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function countVowels(str) {
-  return str.match(/[aeiou]/gi).length;
+function serialize(root) {
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const node = queue.shift();
+    if (node) {
+      result.push(node.val);
+      queue.push(node.left, node.right);
+    } else {
+      result.push(null);
+    }
+  }
+  while (result[result.length - 1] === null) {
+    result.pop();
+  }
+  return result;
 }

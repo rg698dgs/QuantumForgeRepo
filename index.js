0dff1bd313e7b50c7ac2a38a4145283910939d0d
rg698dgs <darthrevepsg@hotmail.com>
2024-05-14 04:01:30 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-function serialize(root) {
-  const result = [];
-  const queue = [root];
-  while (queue.length) {
-    const node = queue.shift();
-    if (node) {
-      result.push(node.val);
-      queue.push(node.left, node.right);
-    } else {
-      result.push(null);
+function numDecodings(s) {
+  const dp = new Array(s.length + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = s[0] === "0" ? 0 : 1;
+  for (let i = 2; i <= s.length; i++) {
+    const oneDigit = parseInt(s.substring(i - 1, i));
+    const twoDigits = parseInt(s.substring(i - 2, i));
+    if (oneDigit >= 1) {
+      dp[i] += dp[i - 1];
+    }
+    if (twoDigits >= 10 && twoDigits <= 26) {
+      dp[i] += dp[i - 2];
     }
   }
-  while (result[result.length - 1] === null) {
-    result.pop();
-  }
-  return result;
+  return dp[s.length];
 }
